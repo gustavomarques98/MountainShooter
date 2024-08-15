28282ea8ca253782a2f054465046843a748ac512
Gustavo Marques <gustavo.marques.cardozo@gmail.com>
2024-08-14 21:30:20 -0300

    creating player and enemy

--- code/Game.py
+++ code/Game.py
@@ -1,18 +1,16 @@
-from code.Const import WIN_HEIGHT, WIN_WIDTH, MENU_OPTION
-import pygame as pygame
-from code.Menu import Menu
+import pygame
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Level import Level
+from code.Menu import Menu
 
 
 class Game:
-
     def __init__(self):
-
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-
         while True:
             menu = Menu(self.window)
             menu_return = menu.run()
@@ -20,8 +18,8 @@ class Game:
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
                 level = Level(self.window, 'Level1', menu_return)
                 level_return = level.run()
-            elif menu_return == MENU_OPTION[3]:
-                print('SCORE')
             elif menu_return == MENU_OPTION[4]:
-                pygame.quit()
-                quit()
+                pygame.quit()  # Close Window
+                quit()  # end pygame
+            else:
+                pass
