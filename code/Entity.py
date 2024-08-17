c28f72db4f6b7b108db9c62cf99f3eb21c0e88d4
Gustavo Marques <gustavo.marques.cardozo@gmail.com>
2024-08-17 18:07:31 -0300

    Revert "creating player and enemy"
    This reverts commit 28282ea8ca253782a2f054465046843a748ac512.

--- code/Entity.py
+++ code/Entity.py
@@ -1,16 +1,15 @@
 from abc import ABC, abstractmethod
-
-import pygame.image
+import pygame as pygame
 
 
 class Entity(ABC):
+
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.surf = pygame.image.load(
-            './assets/' + name + '.png').convert_alpha()
+        self.surf = pygame.image.load(f'./assets/{name}.png')
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
 
     @abstractmethod
-    def move(self, ):
+    def move(self,):
         pass
